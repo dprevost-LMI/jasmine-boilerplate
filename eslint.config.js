f75f4f5f0d2f884f41c324fbeac12daec1446180
David Prevost <david.prevost@goto.com>
2025-07-04 19:55:24 -0400

    upgrade to latest

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,9 @@ import wdioEslint from '@wdio/eslint'
  * This project uses a custom WebdriverIO ESLint configuration for ESLint v9.
  * Feel free to replace this with your own ESLint configuration.
  */
-export default wdioEslint.config()
+export default [
+    ...wdioEslint.config(),
+    {
+        ignores: ['.history/**']
+    }
+]
