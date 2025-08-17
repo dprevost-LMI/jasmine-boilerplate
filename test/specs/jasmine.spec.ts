4e1b3445855edda1d21fa3bc80ecf34dae1519bc
David Prevost <david.prevost@goto.com>
2025-08-17 14:07:52 -0400

    Demonstrate the tsc problem

--- test/specs/jasmine.spec.ts
+++ test/specs/jasmine.spec.ts
@@ -1,9 +1,7 @@
 describe('Jasmine basics', () => {
     it('should be able to use withContext on expect', async () => {
 
-        console.log('expect(\'test\')', expect('test'))
-
-        // The below generate tsc error: Property 'withContext' does not exist on type 'Matchers<void | Promise<void>, string>'.ts(2339)
-        // expect('test').withContext('test').toEqual('test')
+        // The below generate a tsc error, but it works at runtime
+        expect('test').withContext('test').toEqual('test')
     })
 })
